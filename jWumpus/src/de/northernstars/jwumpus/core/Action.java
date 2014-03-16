791c2ac7745e4f5a6887a05946e3db7fde20bd84
Hannes Eilers <hanneseilers@gmx.de>
2014-03-16 10:34:08 +0100

    Categorised actions.

--- jWumpus/src/de/northernstars/jwumpus/core/Action.java
+++ jWumpus/src/de/northernstars/jwumpus/core/Action.java
@@ -1,17 +1,83 @@
 package de.northernstars.jwumpus.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Action {
 
-	MOVE_UP,
-	MOVE_DOWN,
-	MOVE_LEFT,
-	MOVE_RIGHT,
+	MOVE_UP (0),
+	MOVE_DOWN (0),
+	MOVE_LEFT (0),
+	MOVE_RIGHT (0),
 	
-	SHOOT_UP,
-	SHOOT_DOWN,
-	SHOOT_LEFT,
-	SHOOT_RIGHT,
+	SHOOT_UP (1),
+	SHOOT_DOWN (1),
+	SHOOT_LEFT (1),
+	SHOOT_RIGHT (1),
 	
 	NO_ACTION;
 	
+	/**
+	 * {@link List} of all {@link Action} objects that descripe a movement.
+	 */
+	private List<Action> movements = new ArrayList<Action>();		// category = 0
+	
+	/**
+	 * {@link List} of all {@link Action} objects that descripe a shoot.
+	 */
+	private List<Action> shoots = new ArrayList<Action>();			// category = 1
+	
+	/**
+	 * {@link List} of all {@link Action} objects that are not in another category list.
+	 */
+	private List<Action> uncategorised = new ArrayList<Action>();	// category = -1
+	
+	/**
+	 * Constructor to add action to category
+	 * @param category {@link Integer} of category
+	 */
+	private Action(int category){
+		switch(category){
+			case 0:
+				movements.add(this);
+				break;
+				
+			case 1:
+				shoots.add(this);
+				break;
+				
+			default:
+				uncategorised.add(this);
+				break;
+		}
+	}
+	
+	/**
+	 * Consctructor to add uncategorised action.
+	 */
+	private Action(){
+		this(-1);
+	}
+	
+
+	/**
+	 * @return the movements
+	 */
+	protected List<Action> getMovements() {
+		return movements;
+	}
+
+	/**
+	 * @return the shoots
+	 */
+	protected List<Action> getShoots() {
+		return shoots;
+	}
+
+	/**
+	 * @return the uncategorised
+	 */
+	protected List<Action> getUncategorised() {
+		return uncategorised;
+	}
 }
