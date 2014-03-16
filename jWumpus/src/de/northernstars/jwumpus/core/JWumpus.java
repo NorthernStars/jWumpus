1ef485052e0621ef776e6cce1b4a7826cdd4c593
Hannes Eilers <hanneseilers@gmx.de>
2014-03-16 10:14:37 +0100

    Restructured project hirarchy.

--- jWumpus/src/de/northernstars/jwumpus/core/JWumpus.java
+++ jWumpus/src/de/northernstars/jwumpus/core/JWumpus.java
@@ -8,7 +8,6 @@ import javax.swing.JFrame;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import de.northernstars.jwumpus.core.helper.RunnableAI;
 import de.northernstars.jwumpus.gui.MainFrame;
 import de.northernstars.jwumpus.gui.listener.FrameLoadedListener;
 import de.northernstars.jwumpus.gui.listener.JWumpusControl;
--- jWumpus/src/de/northernstars/jwumpus/core/RunnableAI.java
+++ jWumpus/src/de/northernstars/jwumpus/core/RunnableAI.java
@@ -1,4 +1,4 @@
-package de.northernstars.jwumpus.core.helper;
+package de.northernstars.jwumpus.core;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,13 +6,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import de.northernstars.jwumpus.core.Action;
-import de.northernstars.jwumpus.core.ActionSuccess;
-import de.northernstars.jwumpus.core.JWumpus;
-import de.northernstars.jwumpus.core.PlayerState;
-import de.northernstars.jwumpus.core.WumpusMap;
-import de.northernstars.jwumpus.core.WumpusMapObject;
-import de.northernstars.jwumpus.core.WumpusObjects;
 
 public /**
  * Class for processing AI action on extra thread.
